1bc9422fc9dee196f82d5e1fd17d35e859ed424c
Fu Hanxi <fuhanxi@espressif.com>
2021-03-29 11:36:43 +0800

    initial: test gitlab internal package registry

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="pyclang",
+    version="0.0.1",
+    author="Fu Hanxi",
+    author_email="fuhanxi@espressif.com",
+    description="A python clang-tidy runner",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
