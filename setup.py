b8749d67dbd9967072090cf0cf59a04dfc905d0b
Fu Hanxi <fuhanxi@espressif.com>
2021-03-31 19:09:32 +0800

    version: upgrade to 0.0.2

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@ import setuptools
 
 setuptools.setup(
     name='pyclang',
-    version='0.0.1',
+    version='0.0.2',
     author='Fu Hanxi',
     author_email='fuhanxi@espressif.com',
     description='A python clang-tidy runner',
     packages=setuptools.find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',
