2a926e0d42e0f0f89efcbc63042b7ab537f6fdc2
Fu Hanxi <fuhanxi@espressif.com>
2021-08-18 12:41:08 +0800

    bump: update to 0.3.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='pyclang',
-    version='0.3.0',
+    version='0.3.1',
     author='Fu Hanxi',
     author_email='fuhanxi@espressif.com',
     description='A python clang-tidy runner',
