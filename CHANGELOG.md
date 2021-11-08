001d7c5009dff82b51b0dea163ff05070fddcd67
Fu Hanxi <fuhanxi@espressif.com>
2021-11-08 17:17:03 +0800

    publish it on pypi with version 0.1.0

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+## v0.1.0 (2021-11-08)
+
+### Feat
+
+- add idf.py extension
+- add idf_clang to cli
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='pyclang',
-    version='0.4.0-pre.1',
+    version='0.1.0',
     author='Fu Hanxi',
     author_email='fuhanxi@espressif.com',
     description='A python clang-tidy runner',
